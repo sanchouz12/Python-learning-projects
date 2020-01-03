aaeeedfc6259f803bf9774de0bb91f941116efe2
Sanchouz <govnocoder99@gmail.com>
2020-01-03 13:35:05 +0000

    Added 'delete' and 'put'

--- Flask-REST-API/app.py
+++ Flask-REST-API/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from dotenv import load_dotenv
@@ -21,7 +21,20 @@ items = []
 
 
 class Item(Resource):
-    @jwt_required()
+    # belongs to class itself
+    parser = reqparse.RequestParser()
+    parser.add_argument("price",
+                        type = float,
+                        required = True,
+                        help = "You have to specify a price"
+                        )
+    parser.add_argument("name",
+                        type = str,
+                        required = True,
+                        help = "You have to specify a name"
+                        )
+    parser.add_argument("description", type = str)
+
     def get(self, item_id):
         # bad request
         try:
@@ -33,6 +46,7 @@ class Item(Resource):
 
         return {"item": item}, 200 if item else 404
 
+    @jwt_required()
     def post(self, item_id):
         try:
             int(item_id)
@@ -42,7 +56,7 @@ class Item(Resource):
         if next(filter(lambda x: str(x["id"]) == item_id, items), None):
             return {"message": "Item with id '{}' already exists".format(item_id)}, 400
 
-        data = request.get_json()
+        data = Item.parser.parse_args()
 
         item = {
             "id": item_id,
@@ -51,7 +65,34 @@ class Item(Resource):
             "description": data["description"]
         }
         items.append(item)
+
         return {"item": item}, 201
+
+    @jwt_required()
+    def delete(self, item_id):
+        global items
+        items = list(filter(lambda x: x["id"] != item_id, items))
+
+        return {"message": "Item deleted"}
+
+    @jwt_required()
+    def put(self, item_id):
+        data = Item.parser.parse_args()
+
+        item = next(filter(lambda x: x["id"] == item_id, items), None)
+
+        if item:
+            item.update(data)
+        else:
+            item = {
+                "id": item_id,
+                "name": data["name"],
+                "price": data["price"],
+                "description": data["description"]
+            }
+            items.append(item)
+
+        return {"item": item}
 
 
 class ItemList(Resource):
